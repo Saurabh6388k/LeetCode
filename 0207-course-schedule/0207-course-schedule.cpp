201d1bb994d9610a75e85ef78eef1dc27a959a0a
Saurabh Gupta <124552373+Saurabh6388k@users.noreply.github.com>
2026-07-10 00:42:33 +0530

    Time: 12 ms (14.3%), Space: 20.4 MB (13.59%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> temp[numCourses];
+       for(auto it: prerequisites){
+        temp[it[1]].push_back(it[0]);
+       }
+        unordered_map<int,int>indeed;
+        for(int i=0;i<numCourses;i++){
+            for(int j=0;j<temp[i].size();j++){
+            indeed[temp[i][j]]++;
+            }
+        }
+        vector<int> ans;
+        queue<int>fh;
+        for(int i=0;i<numCourses;i++){
+
+        if(!indeed[i]){
+        fh.push(i);
+        }
+        }
+        while(!fh.empty()){
+            int curr=fh.front();
+            fh.pop();
+            ans.push_back(curr);
+            for(int i=0;i<temp[curr].size();i++){
+                indeed[temp[curr][i]]--;
+                if(!indeed[temp[curr][i]])fh.push(temp[curr][i]);
+            }
+        }
+        
+        if(ans.size()==numCourses)return true;
+        return false;
+    }
+};
